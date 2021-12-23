01bb6a8204ab9db7f826311814883ce0b98b5c24
Mariemyoussri <61235438+Mariemyoussri@users.noreply.github.com>
2021-12-24 00:49:56 +0200

    update user done

--- gym-server/src/main/java/com/example/gymserver/controllers/UserController.java
+++ gym-server/src/main/java/com/example/gymserver/controllers/UserController.java
@@ -1,7 +1,10 @@
 package com.example.gymserver.controllers;
 
+import com.example.gymserver.dto.UserDTO;
 import com.example.gymserver.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +18,8 @@ public class UserController {
         this.userService = userService;
     }
 
-
+    @PutMapping("/profile/{userName}")
+    public void updateUserInfo(@RequestBody UserDTO user){
+        userService.updateUserInfo(user);
+    }
 }
