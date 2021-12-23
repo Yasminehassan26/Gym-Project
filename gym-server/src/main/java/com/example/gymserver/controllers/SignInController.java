74093dc1e363cfd9ff73937a43d5e4fcfdf9ea8e
noha-ahmed <noha.morsy2000@gmail.com>
2021-12-23 21:51:23 +0200

    ..

--- gym-server/src/main/java/com/example/gymserver/controllers/SignInController.java
+++ gym-server/src/main/java/com/example/gymserver/controllers/SignInController.java
@@ -5,6 +5,7 @@ import com.example.gymserver.repositories.UserRepository;
 import com.example.gymserver.services.SignInService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,11 +21,23 @@ public class SignInController {
     }
 
     @GetMapping("/sign-in")
-    public Long signIn(@RequestBody SignInDTO signInDTO){
+    public long signIn(@RequestBody SignInDTO signInDTO){
         return this.signInService.signIn(signInDTO.getUserName(),signInDTO.getPassword());
     }
 
     // "api = /sign-in for sign in"
     // "api = /get-user-question/{userName} for get user question"
     // "api = /"
+
+    @GetMapping("/get-user-question/{userName}")
+    public String getUserQuestion(
+        @PathVariable("userName") String userName){
+        return signInService.getUserQuestion(userName);
+    }
+
+    @GetMapping("/validate-answer/{userName}")
+    public long validateAnswer(
+        @PathVariable("userName") String userName, @RequestBody String answer){
+        return signInService.validateAnswer(userName, answer);
+    }
 }
--- gym-server/src/main/java/com/example/gymserver/services/SignInService.java
+++ gym-server/src/main/java/com/example/gymserver/services/SignInService.java
@@ -9,6 +9,10 @@ import java.util.Optional;
 
 @Service
 public class SignInService {
+    public final static String USER_NOT_FOUND_MESSAGE = "User not found!";
+    public final static int USER_NOT_FOUND_STATUS_CODE = -1;
+    public final static int WRONG_PASSWORD_STATUS_CODE = -2;
+    public final static int WRONG_ANSWER_STATUS_CODE = -3;
 
     private UserRepository userRepository;
 
@@ -22,13 +26,13 @@ public class SignInService {
     * @param password
     * @return id of user if found, -1 if the password is wrong, and -2 if user name is not found
     */
-    public Long signIn(String userName, String password){
+    public long signIn(String userName, String password){
         User user = this.userRepository.findUserByUserName(userName).orElse(null);
         if(user == null)
-            return -2L;
+            return USER_NOT_FOUND_STATUS_CODE;
         if(!password.equals(user.getPassword()))
-            return -1L;
-        return user.getId();
+            return WRONG_PASSWORD_STATUS_CODE;
+        return user.getId().longValue();
     }
 
     /*
@@ -38,7 +42,11 @@ public class SignInService {
     * @return the question this user choose when signing up and null if not found
     */
     public String getUserQuestion(String userName){
-        return null;
+        User user = userRepository.findUserByUserName(userName).orElse(null);
+        if( user == null ){
+            return USER_NOT_FOUND_MESSAGE;
+        }
+        return user.getQuestion();
     }
 
     /*
@@ -48,8 +56,14 @@ public class SignInService {
      * @param answer
      * @return user id if the answer is correct and -1 if the answer is not correct
      */
-    public Long validateAnswer(String userName, String answer){
-        return 0L;
+    public long validateAnswer(String userName, String answer){
+        User user = userRepository.findUserByUserName(userName).orElse(null);
+        if( user == null )
+            return USER_NOT_FOUND_STATUS_CODE;
+        if( answer.equals(user.getAnswer()) )
+            return user.getId();
+        else
+            return WRONG_ANSWER_STATUS_CODE;
     }
 
     /*
