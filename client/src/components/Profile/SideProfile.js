c49f63fcdeb4730364cc9a6d5fdd8d0335c7356b
yasminehassan26 <yasminehassan2621@gmail.com>
2021-12-24 05:48:39 +0200

    .

--- client/src/components/Profile/SideProfile.js
+++ client/src/components/Profile/SideProfile.js
@@ -9,21 +9,35 @@ import Badge from "@mui/material/Badge";
 import { styled } from "@mui/material/styles";
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
-
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import FormControl from "@mui/material/FormControl";
+import FormHelperText from "@mui/material/FormHelperText";
 const Input = styled("input")({
   display: "none",
 });
 export default function SideProfile() {
   const [name, setName] = useState("dida");
-  const [mobilePhone, setMobilePhone ] = useState("2685245624862");
-  const[schedual,setSchedual] = useState(false)
+  const [mobilePhone, setMobilePhone] = useState("2685245624862");
+  const [schedual, setSchedual] = useState(false);
 
-  const[data,setData] = useState([]);
-  const[isPending,setIsPending] = useState(true);
-  const[error,isError] = useState("");
-   
+  const [data, setData] = useState([]);
+  const [isPending, setIsPending] = useState(true);
+  const [error, isError] = useState("");
+
+  const [state, setState] = useState({
+    firstName: "",
+    lastName: "",
+    userName: "",
+    password: "",
+    mobile: "",
+    birthdate: "",
+    question: "",
+    answer: "",
+  });
   // data ,isPending ,isError = useFetch()
   const handleSchedual = () => {
     setSchedual(!schedual);
@@ -32,8 +46,20 @@ export default function SideProfile() {
     //fetch the data to show userName and phone number
   }, []);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  const handleChange = (prop) => (event) => {
+    setState({ ...state, [prop]: event.target.value });
+  };
+
   return (
-   
     <Stack
       direction="column"
       justifyContent="center"
@@ -41,7 +67,12 @@ export default function SideProfile() {
       margin={5}
       spacing={3}
     >
-       {isPending &&  <Box sx={{ display: 'flex' }}> <CircularProgress style={{color : "#cc1b85" }}/> </Box> }
+      {isPending && (
+        <Box sx={{ display: "flex" }}>
+          {" "}
+          <CircularProgress style={{ color: "#cc1b85" }} />{" "}
+        </Box>
+      )}
       <Badge
         overlap="circular"
         anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
@@ -49,7 +80,7 @@ export default function SideProfile() {
           <label htmlFor="icon-button-file">
             <Input accept="image/*" id="icon-button-file" type="file" />
             <IconButton
-              style={{color:"#cc1b85"}}
+              style={{ color: "#cc1b85" }}
               aria-label="upload picture"
               component="span"
             >
@@ -61,52 +92,150 @@ export default function SideProfile() {
         <Avatar
           alt="Remy Sharp"
           src="/static/images/avatar/1.jpg"
-          sx={{ width: 100, height: 100 ,color:"#cc1b85"}}
+          sx={{ width: 100, height: 100, color: "#cc1b85" }}
         />{" "}
       </Badge>
-      <Grid        
-        direction="row"
-        style={{ left : 0 , marginLeft: "30%" }}
-        container
-        spacing={1}>
-        <Grid  >
-          <Grid
-           direction = "column"
-           container
-         spacing={1} >
-      <h4>First Name</h4>
-      <TextField label="First Name" defaultValue={name} />
-      </Grid  >
-      <Grid >
-      <h4>Second Name</h4>
-      <TextField label="Second Name" defaultValue={name} />
-      </Grid>
-      <Grid >
-      <h4>User Name</h4>
-      <TextField label="Second Name" defaultValue={name} />
-      </Grid>
-      <Grid >
-      <h4>MobilePhone</h4>
-
-      <TextField label="Mobile Phone" defaultValue={mobilePhone} />
-      </Grid>
-      <Grid >
-      <h4>Question</h4>
-
-      <TextField label="Question" defaultValue={mobilePhone} />
-      </Grid>
-      <Grid >
-      <h4>answer</h4>
-
-      <TextField label="Answer" defaultValue={mobilePhone} />
-      </Grid>
-      </Grid>
-      <Grid
-      direction = "column"
-      style={{marginLeft:"30%"}}>
-      <Button  variant="contained" style={{mt: 3, mb: 2 , backgroundColor : "#cc1b85",border:"solid"}} onClick={handleSchedual}> Show Schedual</Button>
-      {schedual && <div style={{marginTop : "10%"}}>The Schedual is empty right now </div>}
-      </Grid>
+      <Grid direction="row" container spacing={1}>
+        <Grid>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <h4>First Name</h4>
+                <TextField
+                  autoComplete="fname"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <h4>Last Name</h4>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="lname"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <h4>Username</h4>
+                <TextField
+                  disabled
+                  fullWidth
+                  id="userName"
+                  label="Username"
+                  name="username"
+                  autoComplete="username"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <h4>password</h4>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <h4>Mobile number</h4>
+                <TextField
+                  autoComplete="fname"
+                  name="mobile"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="Mobile Number"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <h4>Birthdate</h4>
+                <TextField
+                  id="date"
+                  label="Birthday"
+                  type="date"
+                  name="birthdate"
+                  defaultValue="2022-01-01"
+                  sx={{ width: 267 }}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl>
+                  <InputLabel id="demo-simple-select-helper-label">
+                    Question
+                  </InputLabel>
+                  <Select
+                    required
+                    labelId="demo-simple-select-helper-label"
+                    id="demo-simple-select-helper"
+                    value={state.question}
+                    name="question"
+                    label="Question"
+                    onChange={handleChange("question")}
+                  >
+                    <MenuItem value={"what is your favorite pet?"}>
+                      what is your favorite pet?
+                    </MenuItem>
+                    <MenuItem value={"what is your favorite color?"}>
+                      what is your favorite color?
+                    </MenuItem>
+                  </Select>
+                  <FormHelperText>
+                    (choose security question to restore password)
+                  </FormHelperText>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <h4>answer</h4>
+                <TextField
+                  required
+                  fullWidth
+                  id="answer"
+                  label="answer"
+                  name="answer"
+                  autoComplete="answer"
+                />
+              </Grid>
+            </Grid>
+          </Box>
+        </Grid>
+        <Grid direction="column" style={{ marginLeft: "30%" }}>
+          <Button
+            variant="contained"
+            style={{
+              mt: 3,
+              mb: 2,
+              backgroundColor: "#cc1b85",
+              border: "solid",
+            }}
+            onClick={handleSchedual}
+          >
+            {" "}
+            Show Schedual
+          </Button>
+          {schedual && (
+            <div style={{ marginTop: "10%" }}>
+              The Schedual is empty right now{" "}
+            </div>
+          )}
+        </Grid>
       </Grid>
     </Stack>
   );
--- client/src/components/Registeration/SignUpPage.js
+++ client/src/components/Registeration/SignUpPage.js
@@ -15,14 +15,14 @@ import Select from "@mui/material/Select";
 import FormControl from "@mui/material/FormControl";
 import FormHelperText from "@mui/material/FormHelperText";
 
-
-
 export default function SignUp() {
   const [state, setState] = React.useState({
     firstName: "",
     lastName: "",
     userName: "",
     password: "",
+    mobile:"",
+    birthdate:"",
     question: "",
     answer: "",
   });
@@ -41,139 +41,158 @@ export default function SignUp() {
   };
 
   return (
-  
-      <Grid container component="main" sx={{ height: "100vh" }}>
-        <CssBaseline />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: "#cc1b85" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign up
-            </Typography>
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 3 }}
-            >
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    autoComplete="fname"
-                    name="firstName"
-                    required
-                    fullWidth
-                    id="firstName"
-                    label="First Name"
-                    autoFocus
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="lastName"
-                    label="Last Name"
-                    name="lastName"
-                    autoComplete="lname"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="userName"
-                    label="Username"
-                    name="username"
-                    autoComplete="username"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="new-password"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <FormControl>
-                    <InputLabel id="demo-simple-select-helper-label">
-                      Question
-                    </InputLabel>
-                    <Select
-                      required
-                      labelId="demo-simple-select-helper-label"
-                      id="demo-simple-select-helper"
-                      value={state.question}
-                      name="question"
-                      label="Question"
-                      onChange={handleChange("question")}
-                    >
-                      <MenuItem value="">
-                        <em>None</em>
-                      </MenuItem>
-                      <MenuItem value={"what is your favorite pet?"}>
-                        what is your favorite pet?
-                      </MenuItem>
-                      <MenuItem value={"what is your favorite color?"}>
-                        what is your favorite color?
-                      </MenuItem>
-                    </Select>
-                    <FormHelperText>
-                      (choose security question to restore password)
-                    </FormHelperText>
-                  </FormControl>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="answer"
-                    label="answer"
-                    name="answer"
-                    autoComplete="answer"
-                  />
-                </Grid>
-              </Grid>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2, backgroundColor:"#cc1b85" }}
-              >
-                Sign Up
-              </Button>
-            </Box>
-          </Box>
-        </Grid>
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
+    <Grid container component="main" sx={{ height: "100vh" }}>
+      <CssBaseline />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Box
           sx={{
-            backgroundImage: "url(https://source.unsplash.com/featured/?healthy)",
-            backgroundRepeat: "no-repeat",
-          
-            backgroundSize: "cover",
-            backgroundPosition: "center",
+            my: 8,
+            mx: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
-        />
+        >
+          <Avatar sx={{ m: 1, bgcolor: "#cc1b85" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="fname"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="lname"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="userName"
+                  label="Username"
+                  name="username"
+                  autoComplete="username"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="fname"
+                  name="mobile"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="Mobile Number"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="date"
+                  label="Birthday"
+                  type="date"
+                  name="birthdate"
+                  defaultValue="2022-01-01"
+                  sx={{ width: 267 }}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl>
+                  <InputLabel id="demo-simple-select-helper-label">
+                    Question
+                  </InputLabel>
+                  <Select
+                    required
+                    labelId="demo-simple-select-helper-label"
+                    id="demo-simple-select-helper"
+                    value={state.question}
+                    name="question"
+                    label="Question"
+                    onChange={handleChange("question")}
+                  >
+                    <MenuItem value={"what is your favorite pet?"}>
+                      what is your favorite pet?
+                    </MenuItem>
+                    <MenuItem value={"what is your favorite color?"}>
+                      what is your favorite color?
+                    </MenuItem>
+                  </Select>
+                  <FormHelperText>
+                    (choose security question to restore password)
+                  </FormHelperText>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="answer"
+                  label="answer"
+                  name="answer"
+                  autoComplete="answer"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2, backgroundColor: "#cc1b85" }}
+            >
+              Sign Up
+            </Button>
+          </Box>
+        </Box>
       </Grid>
-   
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage: "url(https://source.unsplash.com/featured/?healthy)",
+          backgroundRepeat: "no-repeat",
+
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+    </Grid>
   );
 }
