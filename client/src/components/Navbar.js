98e3e565934e9a70a798874234c7a82d343a03c0
yasminehassan26 <yasminehassan2621@gmail.com>
2022-01-02 21:18:59 +0200

    .

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,6 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
-
 import SideMenu from "./Profile/SideMenu";
 
 import AssignmentIndSharpIcon from "@mui/icons-material/AssignmentIndSharp";
