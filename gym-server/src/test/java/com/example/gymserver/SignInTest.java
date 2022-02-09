845aa6ba39b832d5d7788cde3a8a59c6da306ddc
noha-ahmed <noha.morsy2000@gmail.com>
2022-02-09 03:42:48 +0200

    Test Database added.

--- gym-server/src/test/java/com/example/gymserver/SignInTest.java
+++ gym-server/src/test/java/com/example/gymserver/SignInTest.java
@@ -6,14 +6,30 @@ import com.example.gymserver.dto.UserIdDTO;
 import com.example.gymserver.models.User;
 import com.example.gymserver.repositories.UserRepository;
 import com.example.gymserver.services.UserService;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class SignInTest {
+    private static boolean setUpIsDone = false;
+
+    @BeforeClass
+    public void setUp() {
+        if (setUpIsDone) {
+            return;
+        }
+        // do the setup
+        setUpIsDone = true;
+        System.out.println("hi");
+    }
 
     @Autowired
     private SignInController signInController ;
@@ -22,6 +38,7 @@ public class SignInTest {
 
     @Test
     public void correctSignInTest(){
+        System.out.println(setUpIsDone);
         SignInDTO signInDTO = new SignInDTO();
         signInDTO.setUserName("mariam");
         signInDTO.setPassword("12345");
